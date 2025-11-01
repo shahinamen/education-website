7f413798ae9cc2fd2a33322f18eba33be71d66b1
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-11-01 12:27:09 +0600

    footer icon added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,15 @@
-import { BookOpen } from "lucide-react";
+import {
+  Book,
+  BookAlert,
+  BookOpen,
+  BookOpenCheck,
+  Contact,
+  Home,
+  Notebook,
+  Settings,
+  User,
+  Waypoints,
+} from "lucide-react";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
@@ -16,7 +27,7 @@ export default function Footer() {
                 </span>
               </Link>
             </div>
-            <p className="text-sm text-gray-400">
+            <p className="text-sm text-white">
               We are a company dedicated to providing the best solutions for
               your needs. Our mission is to deliver quality and excellence in
               every project.
@@ -27,25 +38,43 @@ export default function Footer() {
           <div className="md:w-3/4 grid grid-cols-1 sm:grid-cols-3 gap-8">
             {/* Important Links */}
             <div>
-              <h3 className="text-white font-semibold mb-4">Important Links</h3>
-              <ul className="space-y-2 text-sm">
+              <h3 className="text-orange-600 font-semibold mb-4">
+                Important Links
+              </h3>
+              <ul className="space-y-2 text-medium text-white">
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600 flex items-center"
+                  >
+                    <Home size={24} className="pr-2" />
                     Home
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600 flex items-center"
+                  >
+                    <Settings size={24} className="pr-2" />
                     Services
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600 flex items-center"
+                  >
+                    <User size={24} className="pr-2" />
                     About Us
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600 flex items-center"
+                  >
+                    <Contact size={24} className="pr-2" />
                     Contact
                   </a>
                 </li>
@@ -54,21 +83,42 @@ export default function Footer() {
 
             {/* Policy */}
             <div>
-              <h3 className="text-white font-semibold mb-4">Policy</h3>
-              <ul className="space-y-2 text-sm">
+              <h3 className="text-orange-600 font-semibold mb-4">Policy</h3>
+              <ul className="space-y-2 text-sm text-white">
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600  flex items-center"
+                  >
+                    <BookOpenCheck size={24} className="pr-2" />
                     Privacy Policy
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600  flex items-center"
+                  >
+                    <BookAlert size={24} className="pr-2" />
                     Terms of Service
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600  flex items-center"
+                  >
+                    <Book size={24} className="pr-2" />
                     Cookie Policy
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="hover:text-orange-600  flex items-center"
+                  >
+                    <Notebook size={24} className="pr-2" />
+                    Usage Policy
                   </a>
                 </li>
               </ul>
@@ -76,25 +126,43 @@ export default function Footer() {
 
             {/* Social Media */}
             <div>
-              <h3 className="text-white font-semibold mb-4">Social Media</h3>
-              <ul className="space-y-2 text-sm">
+              <h3 className="text-orange-600 font-semibold mb-4">
+                Social Media
+              </h3>
+              <ul className="space-y-2 text-sm text-white">
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600  flex items-center"
+                  >
+                    <Waypoints size={24} className="pr-2" />
                     Facebook
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600  flex items-center"
+                  >
+                    <Waypoints size={24} className="pr-2" />
                     Twitter
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600  flex items-center"
+                  >
+                    <Waypoints size={24} className="pr-2" />
                     Instagram
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-white">
+                  <a
+                    href="#"
+                    className="hover:text-orange-600  flex items-center"
+                  >
+                    <Waypoints size={24} className="pr-2" />
                     LinkedIn
                   </a>
                 </li>
@@ -103,7 +171,7 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className="p-4 text-center text-gray-500 border-t mt-4">
+      <div className="p-4 text-center text-gray-500 border-t border-orange-600 mt-4">
         © {new Date().getFullYear()} Universal. All rights reserved.
       </div>
     </footer>
